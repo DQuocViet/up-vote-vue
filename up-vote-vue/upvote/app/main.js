1bc139b28b133316cdd67bbf9cdf4169562356b9
hopee-do <ntdo@hopee.com.vn>
2022-07-29 12:23:28 +0900

    up-vote-vue

---
+++ up-vote-vue/upvote/app/main.js
@@ -0,0 +1,26 @@
+// import
+import Seed from "./seed.js"
+
+// new vue instance
+var vue = new Vue({
+    el: '#app',
+    data: {
+        submissions: Seed.submissions
+    },
+    computed: {
+        sortedSubmissions() {
+            return this.submissions.sort((a, b) => {
+                return b.votes - a.votes
+            });
+        }
+    },
+    methods: {
+        upvote(submissionId) {
+            const submission = this.submissions.find(
+                submission => submission.id === submissionId);
+            submission.votes++;
+        }
+    },
+});
+
+
---
+++ up-vote-vue/upvote/app/seed.js
@@ -0,0 +1,43 @@
+const submissions = [
+    {
+        id: 1,
+        title: 'Yellow Pail',
+        description: 'On-demand sand castle construction expertise.',
+        url: '#',
+        votes: 16,
+        avatar: '../public/images/avatars/daniel.jpg',
+        submissionImage: '../public/images/submissions/image-yellow.png'
+    },
+    {
+        id: 2,
+        title: 'Suppermajority: The Fantasy Congress League',
+        description: 'Earn points when your favorite politicians pass legislation.',
+        url: '#',
+        votes: 11,
+        avatar: '../public/images/avatars/daniel1.jpg',
+        submissionImage: '../public/images/submissions/image-yellow1.png'
+    },
+    {
+        id: 3,
+        title: 'Tinfoild: Tailored tinfoil hats',
+        description: 'We have your measurements and shipping address.',
+        url: '#',
+        votes: 20,
+        avatar: '../public/images/avatars/daniel2.jpg',
+        submissionImage: '../public/images/submissions/image-yellow2.png'
+    },
+    {
+        id: 4,
+        title: 'Haught or Naught',
+        description: 'High-minded or absent-minded? You decide.',
+        url: '#',
+        votes: 9,
+        avatar: '../public/images/avatars/daniel3.jpg',
+        submissionImage: '../public/images/submissions/image-yellow3.png'
+    },
+// ...
+]
+
+export default {
+    submissions
+}
